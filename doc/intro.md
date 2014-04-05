1e4642a6bd0e74618fc73aafad08e762b7e2ec6a
Shriphani Palakodety <shriphanip@gmail.com>
2014-04-04 20:00:18 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to armies_dataset
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/armies_dataset/core_test.clj
@@ -0,0 +1,7 @@
+(ns armies-dataset.core-test
+  (:require [clojure.test :refer :all]
+            [armies-dataset.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
